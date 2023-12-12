2024f59e5d91b6d7270f1a2a2e71c2e972e5e8c4
zawe <techpeanuthome@gmail.com>
2023-12-12 09:38:06 +0630

    Category Detail Ui Update

--- src/components/Dashboard/Category/CategoryDetail.jsx
+++ src/components/Dashboard/Category/CategoryDetail.jsx
@@ -5,6 +5,7 @@ import { TbEdit } from "react-icons/tb";
 import FadeLoader from "react-spinners/FadeLoader";
 import { useDispatch, useSelector } from "react-redux";
 import { removeData } from "../../../redux/actions";
+import { Icon } from "@iconify/react";
 
 export default function CategoryDetail() {
   const { id } = useParams();
@@ -34,69 +35,67 @@ export default function CategoryDetail() {
 
   return (
     <>
-      <div className="flex justify-between items-center cursor-pointer">
-        <Link to="/admin/categorys/all">
-          <button className="hover:opacity-75 text-sm px-6 py-1.5 text-white bg-blue-600 rounded-sm shadow-md border-2 border-blue-600 hover:opacity-75text-white">
+      <div className="flex justify-between">
+        <div className="flex gap-2">
+          <Link
+            to="/admin/products/all"
+            className="font-bold rounded-sm shadow-sm flex items-center text-gray-700 border-gray-500 border-2 hover:opacity-75 text-sm hover:text-white hover:bg-gray-500 px-3 py-1.5"
+          >
             Back
-          </button>
-        </Link>
-        <Link to={`/admin/categorys/edit/${id}`}>
-          <TbEdit className="text-4xl font-bold text-blue-700 hover:text-slate-700" />
-        </Link>
+          </Link>
+        </div>
       </div>
 
-      {detail ? (
-        <div className="text-center">
-          {detail && detail.length > 0 ? (
-            <div className="w-full">
-              <h2 className="py-1.5 text-md text-start font-bold my-4 bg-blue-600 text-white pl-4">
-                Category Information
-              </h2>
-              <div className="flex justify-between">
-                <div className="w-2/4">
-                  <div className="flex justify-between my-3">
-                    <h4 className="font-bold text-lg text-slate-500">
-                      Category Name
-                    </h4>
-                    <h3 className="font-bold text-lg text-slate-600 w-3/5 mr-20 pl-3 py-2 rounded-md bg-slate-100">
-                      {detail[0].name}
-                    </h3>
-                  </div>
-                  <div className="flex justify-between my-3">
-                    <h4 className="font-bold text-lg text-slate-500">
-                      Created Date
-                    </h4>
-                    <h3 className="font-bold text-lg text-blue-600 w-3/5 mr-20 pl-3 py-2 rounded-md bg-slate-100 ">
-                      {new Date(detail[0].createdAt).toLocaleDateString(
-                        "en-US"
-                      )}
-                    </h3>
-                  </div>
-                </div>
-
-                <div className="w-2/4 justify-between">
-                  <div className="flex justify-between my-3">
-                    <h4 className="font-bold text-lg text-slate-500">
-                      Updated Date
-                    </h4>
-                    <h3 className="font-bold text-lg text-slate-600 w-3/5 mr-20 pl-3 py-2 rounded-md bg-slate-100 ">
-                      {new Date(detail[0].updatedAt).toLocaleDateString(
-                        "en-US"
-                      )}
-                    </h3>
-                  </div>
-                  <div className="flex justify-between my-3">
-                    <h4 className="font-bold text-lg text-slate-500">Active</h4>
-                    <h3 className="font-bold text-lg text-slate-600 w-3/5 mr-20 pl-3 py-2 rounded-md bg-slate-100 ">
-                      {detail[0].active ? "Yes" : "No"}
-                    </h3>
-                  </div>
-                </div>
+      {detail && detail.length > 0 ? (
+        <div className="container my-5">
+          <h2 className="lg:text-xl font-bold my-2">Category Information</h2>
+          <div className="container bg-white p-5 rounded-lg max-w-6xl">
+            <div className="flex">
+           
+              <div className="ml-auto">
+                <Link
+                  to={`/admin/products/edit/${id}`}
+                >
+                  <Icon icon="ep:edit" className="text-xl" />
+                </Link>
               </div>
             </div>
-          ) : (
-            <div>Loading...</div>
-          )}
+
+            <div className="grid grid-cols-2 max-w-3xl gap-10 my-10">
+              <div className="container space-y-8 font-semibold text-sm">
+                <div className="flex justify-between items-center">
+                  <h4>Category Name</h4>
+                  <h3 className="font-medium">
+                    {detail[0].name ? detail[0].name.toUpperCase() : ""}
+                  </h3>
+                </div>
+           
+                <div className="flex justify-between items-center">
+                  <h4>Created Date</h4>
+                  <h3 className="font-medium">
+                    {detail[0].createdAt
+                      ? new Date(detail[0].createdAt).toLocaleDateString()
+                      : ""}
+                  </h3>
+                </div>
+             
+            
+              </div>
+              <div className="container space-y-8 font-semibold text-sm">
+                <div className="flex justify-between items-center">
+                  <h4>Active</h4>
+                  <h3 className="font-medium">  {detail[0].active ? "Yes" : "No"}</h3>
+                </div>
+                <div className="flex justify-between items-center">
+                  <h4>Updated Date</h4>
+                  <h3 className="font-medium">{detail[0].updatedAt
+                    ? new Date(detail[0].updatedAt).toLocaleDateString()
+                    : ""}</h3>
+                </div>
+              </div>
+          
+            </div>
+          </div>
         </div>
       ) : (
         <div className="flex items-center justify-center mt-40">
