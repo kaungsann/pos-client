99bb97b0930d0fd981cf01eeca81745ec4abb8cc
zawe <techpeanuthome@gmail.com>
2023-12-13 13:45:17 +0630

    EmployeeEdit Ui Update

--- src/components/Dashboard/Employee/EmployeeEdit.jsx
+++ src/components/Dashboard/Employee/EmployeeEdit.jsx
@@ -6,6 +6,8 @@ import { removeData } from "../../../redux/actions";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { format } from "date-fns";
+import { Input, Select, SelectItem } from "@nextui-org/react";
+
 
 export default function EmployeeEdit() {
   const { id } = useParams();
@@ -104,218 +106,107 @@ export default function EmployeeEdit() {
         theme="light"
         style={{ width: "450px" }}
       />
-      <div className="flex min-h-full w-full flex-col">
-        <div className="mb-3 pb-6 border-b-2 border-b-slate-300 flex justify-between">
-          <h2 className="text-2xl font-bold text-slate-700 ">
-            Edit Employee Information
-          </h2>
-          <Link to="/admin/employee/all">
-            <div className="font-bold rounded-sm shadow-sm flex items-cente text-blue-700 border-blue-500 border-2 hover:opacity-75 text-md hover:text-white hover:bg-blue-700 px-6 py-2">
-              Back
-            </div>
-          </Link>
-        </div>
-        <form
-          className=" w-full flex flex-wrap items-center"
-          action="#"
-          method="POST"
-        >
-          <div className="w-80 my-2 mr-1">
-            <label
-              htmlFor="name"
-              className="after:content-['*'] mb-3 after:ml-0.5 after:text-red-500 block text-lg font-semibold text-slate-600"
-            >
-              Name
-            </label>
-            <div className="mt-2">
-              <input
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                id="name"
-                name="name"
-                type="text"
-                required
-                className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
-              />
-            </div>
-          </div>
-
-          <div className="w-80 my-2 mr-1">
-            <label
-              htmlFor="email"
-              className="after:content-['*'] mb-3 after:ml-0.5 after:text-red-500 block text-lg font-semibold text-slate-600"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                id="email"
-                name="email"
-                type="email"
-                required
-                className=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
-              />
-            </div>
-          </div>
-
-          <div className="w-80 my-2 mr-1">
-            <label
-              htmlFor="city"
-              className="after:content-['*'] mb-3 after:ml-0.5 after:text-red-500 block text-lg font-semibold text-slate-600"
-            >
-              Contact Address
-            </label>
-            <div className="mt-2">
-              <input
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                id="address"
-                name="address"
-                type="text"
-                required
-                className=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
-              />
-            </div>
-          </div>
-
-          <div className="w-80 my-2 mr-1">
-            <label
-              htmlFor="gender"
-              className="after:content-['*'] mb-3 after:ml-0.5 after:text-red-500 block text-lg font-semibold text-slate-600"
-            >
-              Date Of Birth
-            </label>
-            <div className="mt-2">
-              <input
-                value={birth}
-                onChange={(e) => setBirth(e.target.value)}
-                id="birth"
-                name="birth"
-                type="date"
-                required
-                className=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
-              />
-            </div>
-          </div>
-
-          <div className="w-80 my-2 mr-1">
-            <label
-              htmlFor="phone"
-              className="after:content-['*'] mb-3 after:ml-0.5 after:text-red-500 block text-lg font-semibold text-slate-600"
-            >
-              Phone Number
-            </label>
-            <div className="mt-2">
-              <input
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                id="phone"
-                name="phone"
-                type="number"
-                required
-                className=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
-              />
-            </div>
-          </div>
-
-          <div className="w-80 my-2 mr-1">
-            <label
-              htmlFor="gender"
-              className="after:content-['*'] mb-3 after:ml-0.5 after:text-red-500 block text-lg font-semibold text-slate-600"
-            >
-              Gender
-            </label>
-            <div className="mt-2">
-              <select
-                value={gender}
-                onChange={(e) => setGender(e.target.value)}
-                id="gender"
-                required
-                name="gender"
-                className=" px-2 block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
-              >
-                <option disabled value>
-                  Select an option
-                </option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-              </select>
-            </div>
-          </div>
-
-          <div className="w-80 my-2 mr-1">
-            <label
-              htmlFor="city"
-              className="after:content-['*'] mb-3 after:ml-0.5 after:text-red-500 block text-lg font-semibold text-slate-600"
-            >
-              City
-            </label>
-            <div className="mt-2">
-              <select
-                value={city}
-                id="city"
-                onChange={(e) => setCity(e.target.value)}
-                name="city"
-                className=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
-              >
-                <option disabled value>
-                  Select an option
-                </option>
-                <option value="Yagon">Yagon</option>
-                <option value="Mandalay">Mandalay</option>
-                <option value="PyiOoLwin">PyiOoLwin</option>
-                <option value="Pago">Bago</option>
-              </select>
-            </div>
-          </div>
-        </form>
+      <div className="flex gap-3 my-5">
         <button
+          type="submit"
+          className="font-bold rounded-sm shadow-sm flex items-center text-blue-700 border-blue-500 border-2 hover:opacity-75 text-sm hover:text-white hover:bg-blue-700 px-3 py-1.5"
           onClick={handleSubmit}
-          className="w-80 my-3 items-center flex justify-center rounded-md bg-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Submit
+          Save
         </button>
+        <Link to="/admin/products/all">
+          <button className="rounded-sm shadow-sm flex items-center  text-red-500 border-red-500 bg-white border-2 hover:opacity-75 text-sm hover:text-white hover:bg-red-500 font-bold px-3 py-1.5">
+            Discard
+          </button>
+        </Link>
       </div>
 
-      {/* <div className="w-96 absolute top-32 left-0 right-0 z-50 mx-auto bg-white rounded-md shadow-md flex justify-center">
-        {showBox && (
-          <div className="w-72 my-3">
-            <div className="mt-2">
-              <div className="w-80 my-2 mr-1">
-                  <div className="flex justify-between">
-                    <label
-                      htmlFor="phone"
-                      className="after:content-['*'] mb-3 after:ml-0.5 after:text-red-500 block text-lg font-semibold text-slate-600"
-                    >
-                      Password*
-                    </label>
-                    <h3 onClick={() => setShowBox(false)} className="text-slate-600 font-semibold cursor-pointer text-xl hover:text-slate-400">X</h3>
-                  </div>
-                <div className="mt-2">
-                  <input
-                    onChange={(e) => setPassword(e.target.value)}
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    placeholder="enter admin password"
-                    className=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
-                  />
-                </div>
+      <div className="container mt-2">
+        <h2 className="lg:text-xl font-bold my-2">Employee Edit</h2>
+        <div className="container bg-white p-5 rounded-lg max-w-6xl">
+          <form className="flex justify-between gap-10 p-5">
+
+            <div className="flex flex-wrap gap-8">
+              <div className="w-60">
+                <Input
+                  type="text"
+                  label="Name"
+                  name="name"
+                  value={name}
+                  // color={isInvalid ? "danger" : "success"}
+                  // errorMessage={isInvalid && "Please enter a valid email"}
+                  onChange={(e) => handleSubmit(e)}
+                  placeholder="Enter  name..."
+                  labelPlacement="outside"
+                />
               </div>
-              <button
-                onClick={handleSubmit}
-                className="w-72 my-3 items-center flex justify-center rounded-md bg-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              <div className="w-60">
+                <Input
+                  type="text"
+                  name="email"
+                  label="Email"
+                  value={email}
+                  onChange={(e) => handleSubmit(e)}
+                  placeholder="Enter Email..."
+                  labelPlacement="outside"
+                />
+              </div>
+
+              <div className="w-60">
+                <Input
+                  type="text"
+                  isDisabled
+                  name="address"
+                  label="Address"
+                  value={address}
+                  placeholder="Enter Address..."
+                  labelPlacement="outside"
+                />
+              </div>
+              <div className="w-60">
+                <span className="text-sm">DOB</span>
+                <Input
+                  type="date"
+                  name="dob"
+                  labelPlacement="outside"
+                  value={
+                    birth
+                      ? new Date(birth).toISOString().split("T")[0]
+                      : ""
+                  }
+                  onChange={(e) => handleSubmit(e)}
+                />
+              </div>
+              <div className="w-60">
+                <Input
+                  type="text"
+                  name="phone"
+                  label="Phone"
+                  value={phone}
+                  onChange={(e) => handleSubmit(e)}
+                  placeholder="Enter Phone..."
+                  labelPlacement="outside"
+                />
+              </div>
+              <div className="w-60">
+                <Select
+                  labelPlacement="outside"
+                  label="Gender"
+                  name="gender"
+                  value={gender}
+                  placeholder="Select an gender"
+                  selectedKeys={gender ? [gender] : false}
+                  onChange={(e) => handleSubmit(e.target.value)}
+                  className="max-w-xs"
                 >
-                Submit
-              </button>
+                  <SelectItem value="male" key="male">male</SelectItem>
+                  <SelectItem value="female" key="female">female</SelectItem>
+                </Select>
+              </div>
             </div>
-          </div>
-        )}
-      </div> */}
+          </form>
+        </div>
+      </div>
     </>
   );
 }
