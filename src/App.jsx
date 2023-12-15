e13e3cda633a6533cd4cd8258f4f3bf6aca709e2
kaungsann <kaungsanh53@gmail.com>
2023-12-15 16:00:21 +0630

    change file import

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ import Warehouse from "./components/Dashboard/WareHouse/Warehouse";
 import SaleView from "./components/Dashboard/SaleOrder/SaleView";
 import ProductTemplate from "./components/Product/ProductTemplate";
 import CategoryTemplate from "./components/Category/CategoryTemplate";
-import StockTemplate from "./components/Dashboard/Stock/StockTemplate";
+import StockTemplate from "./components/Stock/StockTemplate";
 import AdjustmentTemplate from "./components/Adjustment/AdjustmentTemplate";
 import PurchaseTemplate from "./components/Dashboard/Purchase/PurchaseTemplate";
 import SaleTemplate from "./components/Dashboard/SaleOrder/SaleTemplate";
@@ -120,7 +120,7 @@ function App() {
               <Route path="vendors" element={<VendorTemplate />} />
             </Route>
             <Route path="locations">
-              <Route path="all" element={<LocationTemplate/>} />
+              <Route path="all" element={<LocationTemplate />} />
               <Route path="create" element={<LocationCreate />} />
               <Route path="edit/:id" element={<LocationEdit />} />
               <Route path="detail/:id" element={<LocationDetail />} />
